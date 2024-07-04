a5a5f29eef798615fdfe8dcbfaa795edeb4ff08c
saruni <timothysaruni13@gmail.com>
2024-07-05 00:06:40 +0300

    adds seeding

--- server/seed.py
+++ server/seed.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python3
-#server/seed.py
-
 from app import app
 from models import db, Pet
 
 with app.app_context():
+
+    # Delete all rows in the "pets" table
+    Pet.query.delete()
 
     # Create an empty list
     pets = []
@@ -13,6 +13,7 @@ with app.app_context():
     pets.append(Pet(name = "Fido", species = "Dog"))
     pets.append(Pet(name = "Whiskers", species = "Cat"))
     pets.append(Pet(name = "Hermie", species = "Hamster"))
+    pets.append(Pet(name = "Slither", species = "Snake"))
 
     # Insert each Pet in the list into the database table
     db.session.add_all(pets)
